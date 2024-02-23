1e0bc22e4d6d7b59541b5321ef339ea78b6e9c6c
Rajaneesh <grajaneesh22@gmail.com>
2024-02-23 17:25:13 +0530

    Fixed screen layout

--- src/components/UrxInfo/template-elements/plg-info.tsx
+++ src/components/UrxInfo/template-elements/plg-info.tsx
@@ -5,8 +5,9 @@ import { UrxElement } from "./urx-element";
 import filter from "lodash/filter";
 import isEmpty from "lodash/isEmpty";
 import { useState } from "react";
+import { Accordion, AccordionItem } from '@carbon/react'
 
-export function PlgInfo(props) {
+export function PlgInfo(props: any) {
   const {
     mainContent,
     productName = "",
@@ -15,8 +16,8 @@ export function PlgInfo(props) {
   } = props;
   const [open, setOpen] = useState(false)
   const { lang = "en" } = mainContent;
-  const headerElements = [];
-  const bodyElements = [];
+  const headerElements: any[] = [];
+  const bodyElements: any[] = [];
   filter(mainContent.v23, (i) => {
     if (["leftHeader", "leftSubHeader"].indexOf(i.id) > -1) {
       headerElements.push(i);
@@ -26,37 +27,51 @@ export function PlgInfo(props) {
   });
   return (
     <>
-      <div className="header-content">
-        {headerElements.map((element) => {
-          const props = {
-            element,
-            productName,
-            lng: lang,
-            key: element.id,
-          };
-          return <UrxElement {...props} />;
-        })}
-      </div>
+
       {!isEmpty(bodyElements) && (
-        <div className={`body-content ${accordion? 'has-accordion': ''}`}>
-          {/* {accordion && (
-            <Accordion className="info-accordion">
-              <AccordionItem open={open} title={`${open ? toggleText.collapse : toggleText.expand }`} onClick={()=>{
-               setOpen(!open);
-              }}>
-                {bodyElements.map((element) => {
+        <div className={`body-content ${accordion ? 'has-accordion' : ''}`}>
+          {accordion && (
+            <div className="mobile-content">
+              <div className="header-content">
+                {headerElements.map((element) => {
                   const props = {
                     element,
+                    productName,
                     lng: lang,
                     key: element.id,
                   };
                   return <UrxElement {...props} />;
                 })}
-              </AccordionItem>
-            </Accordion>
-          )} */}
+              </div>
+              <Accordion className="info-accordion">
+                <AccordionItem open={open} title={`${open ? toggleText.collapse : toggleText.expand}`} onClick={() => {
+                  setOpen(!open);
+                }}>
+                  {bodyElements.map((element) => {
+                    const props = {
+                      element,
+                      lng: lang,
+                      key: element.id,
+                    };
+                    return <UrxElement {...props} />;
+                  })}
+                </AccordionItem>
+              </Accordion>
+            </div>
+          )}
           {!accordion && (
-            <>
+            <div className="desktop-content">
+              <div className="header-content">
+                {headerElements.map((element) => {
+                  const props = {
+                    element,
+                    productName,
+                    lng: lang,
+                    key: element.id,
+                  };
+                  return <UrxElement {...props} />;
+                })}
+              </div>
               {bodyElements.map((element) => {
                 const props = {
                   element,
@@ -65,7 +80,7 @@ export function PlgInfo(props) {
                 };
                 return <UrxElement {...props} />;
               })}
-            </>
+            </div>
           )}
         </div>
       )}
