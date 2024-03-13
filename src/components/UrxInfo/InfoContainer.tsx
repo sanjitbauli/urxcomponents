a6c947a62554aa958063c28a5c1c77ba6b9247e8
Rajaneesh <rajaneesh@in.ibm.com>
2024-03-13 17:36:00 +0530

    Added formId

--- src/components/UrxInfo/InfoContainer.tsx
+++ src/components/UrxInfo/InfoContainer.tsx
@@ -1,48 +1,70 @@
 import { Column, FlexGrid, Row } from "@carbon/react";
 import React, { PureComponent } from "react";
-
-import { MainContentType } from './types/MainContentType';
-import PropTypes from "prop-types";
-import UrxInfo from "./UrxInfo";
 import { getProductName } from "./info.utils";
+import { MainContentType } from './types/MainContentType';
+import UrxInfo from "./UrxInfo";
+import { fetchUrxForm } from "./service";
 
 type Props = {
-  formConfig: {
+  formConfig?: {
     mainContent: MainContentType;
     designTemplate: string;
     assetType: string;
   };
   className: string;
+  formId: string;
 }
-export class InfoContainer extends PureComponent<Props>{
+
+interface State {
+  formConfig?: Props['formConfig']; 
+}
+
+export class InfoContainer extends PureComponent<Props, State>{
   constructor(props: Props) {
     super(props);
+    this.state = {
+      formConfig: props.formConfig 
+    };
   }
-  render() {
-    const { formConfig, className } = this.props;
-    const { assetType, mainContent } = formConfig;
-    const productName = getProductName(formConfig);
 
-    return (<div className={`bg-container ${className}`}>
-      <FlexGrid className="book-container" condensed={false}>
-        <Row>
-          <Column sm={4} md={8} lg={16} className="left-column">
-            <UrxInfo
-              mainContent={mainContent}
-              assetType={assetType}
-              productName={productName}
-              designTemplate={formConfig.designTemplate}
-              t={()=>{}}
-              container={true}
-            />
-          </Column>
-        </Row>
-      </FlexGrid>
-    </div>)
+  componentDidMount(): void {
+
+    fetchUrxForm(this.props.formId, (response: any) => {
+      this.setState({
+        formConfig: response
+      })
+    }, (error: any) => {
+     
+      console.log(error)
+    })
+  }
+
+  render() {
+    const { className } = this.props;
+    const { formConfig } = this.state;
+    const productName = formConfig ? getProductName(formConfig) : "";
+
+    return (
+      <div className={`bg-container ${className}`}>
+        <FlexGrid className="book-container" condensed={false}>
+          <Row>
+            <Column sm={4} md={8} lg={16} className="left-column">
+              {formConfig && ( // Render UrxInfo only if formConfig exists
+                <UrxInfo
+                  mainContent={formConfig.mainContent}
+                  assetType={formConfig.assetType}
+                  productName={productName}
+                  designTemplate={formConfig.designTemplate}
+                  t={(t: any) => { return t; }}
+                  container={true}
+                />
+              )}
+            </Column>
+          </Row>
+        </FlexGrid>
+      </div>
+    );
   }
 }
-InfoContainer.propTypes = {
-  formConfig: PropTypes.object.isRequired,
-};
 
 export default InfoContainer;
